c0e3d388f1f36b2434d6364c785f7609f787d64d
Alan Orozco <alan@orozco.xyz>
2020-11-03 13:18:28 -0800

    Document parameters

--- test/.storybook/preview.js
+++ test/.storybook/preview.js
@@ -1,6 +1,15 @@
 import {addParameters} from '@storybook/preact';
 
 addParameters({
-  localBaseUrl: "https://storage.googleapis.com/amp-test-website-1/amp_dist_82969/dist",
-  localAlternateNames: false,
+  // "Local" URL prefix to fetch JS files for a specific build (eg. dev server)
+  localBaseUrl: "https://localhost:8000/dist",
+
+  // Binary files are named differently than the CDN when running the local dev
+  // server. If the localBaseUrl points to a local dev server, set this to true
+  // (default). Otherwise, JS file paths are like the CDN.
+  // 
+  // localAlternateNames:
+  //      true   amp.js   amp-list-0.1.max.js   ...
+  //      false  v0.js    amp-list-0.1.js       ...
+  localAlternateNames: true,
 });
