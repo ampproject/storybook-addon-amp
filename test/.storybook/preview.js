0f6b093c3fecf6214acfe2781d2ac39365b6f53f
Alan Orozco <alan@orozco.xyz>
2020-11-03 13:19:11 -0800

    wording

--- test/.storybook/preview.js
+++ test/.storybook/preview.js
@@ -4,8 +4,8 @@ addParameters({
   // "Local" URL prefix to fetch JS files for a specific build (eg. dev server)
   localBaseUrl: "https://localhost:8000/dist",
 
-  // Binary files are named differently than the CDN when running the local dev
-  // server. If the localBaseUrl points to a local dev server, set this to true
+  // Binary files are named differently than the CDN when running the `gulp` dev
+  // server. If the localBaseUrl points to this local server, set this to true
   // (default). Otherwise, JS file paths are like the CDN.
   // 
   // localAlternateNames:
